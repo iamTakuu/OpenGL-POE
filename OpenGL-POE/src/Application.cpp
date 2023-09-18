7e2eb8f8168809a26787b3167c20e405d95e54ff
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2023-09-18 11:18:10 +0200

    New terrain improvements

--- OpenGL-POE/src/Application.cpp
+++ OpenGL-POE/src/Application.cpp
@@ -65,14 +65,16 @@ bool InitWindow()
 
 std::vector<GLfloat> terrain_vertices;
 std::vector<GLuint> terrain_indices;
+int nrChannels;
+unsigned char* image = NULL;
 
 // Function to load a heightmap image
 bool loadHeightmap(const char* filename, std::vector<unsigned char>& heightmap, int& width, int& height) {
 	// Load the image using your preferred image loading library
 	// For simplicity, let's assume you have a function for this
 	// For example, using stb_image.h:
-	int channels;
-	unsigned char* image = stbi_load(filename, &width, &height, &channels, 0);
+	
+	image = stbi_load(filename, &width, &height, &nrChannels, 0);
 	if (!image) {
 		std::cerr << "Failed to load heightmap." << std::endl;
 		return false;
@@ -82,31 +84,31 @@ bool loadHeightmap(const char* filename, std::vector<unsigned char>& heightmap, 
 	heightmap.resize(width * height);
 	for (int i = 0; i < width * height; i++) {
 		// Convert RGB to grayscale by averaging channels
-		heightmap[i] = (image[i * channels] + image[i * channels + 1] + image[i * channels + 2]) / 3;
+		heightmap[i] = (image[i * nrChannels] + image[i * nrChannels + 1] + image[i * nrChannels + 2]) / 3;
 	}
 
 	stbi_image_free(image);
 	return true;
 }
 
- 
-
-
 // Function to generate the terrain mesh from the heightmap
 void generateTerrainMesh(const std::vector<unsigned char>& heightmap, int width, int height, int numstrips, int numtrisPerStrip) {
+	
 	// Calculate the vertex positions and store them in the 'vertices' vector
 	float yScale = 64.0f / 256.0f, yShift = 16.0f;
 	int rez = 1;
-	
-	for (int z = 0; z < height; z++) {
-		for (int x = 0; x < width; x++) {
-			float xPos = static_cast<float>(x);
-			float zPos = static_cast<float>(z);
-			float yPos = static_cast<float>(heightmap[z * width + x]) / 10.0f; // Scale the height
+	unsigned bytePerPixel = nrChannels;
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			unsigned char* pixelOffset = image + (j + width * i) * bytePerPixel;
+			unsigned char y = pixelOffset[0];
 
-			terrain_vertices.push_back(xPos);
-			terrain_vertices.push_back(yPos);
-			terrain_vertices.push_back(zPos);
+			// vertex
+			terrain_vertices.push_back(-height / 2.0f + height * i / (float)height);   // vx
+			terrain_vertices.push_back((int)y * yScale - yShift);   // vy
+			terrain_vertices.push_back(-width / 2.0f + width * j / (float)width);   // vz
 		}
 	}
 
@@ -135,8 +137,7 @@ void generateTerrainMesh(const std::vector<unsigned char>& heightmap, int width,
 
 int main()
 {
-	const int numStrips = 0;
-	const int numTrisPerStrip = 0;
+	 
 
 	if (InitWindow()) // If the window was not created
 		return -1;
@@ -146,16 +147,20 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// Create the shader program
-	Shader shaderProgram = Shader("Shaders/default.vert", "Shaders/default.frag");
+	Shader shaderProgram = Shader("Shaders/height.vert", "Shaders/height.frag");
 		
 	// Load the heightmap and generate the terrain mesh
 		std::vector<unsigned char> heightmap;
 		int heightmapWidth, heightmapHeight;
-		if (!loadHeightmap("Height Map/Terrain.png", heightmap, heightmapWidth, heightmapHeight)) {
+		if (!loadHeightmap("Height Map/Terrain2.png", heightmap, heightmapWidth, heightmapHeight)) {
 			glfwTerminate();
 			return -1;
 		}
-		generateTerrainMesh(heightmap, heightmapWidth, heightmapHeight, numStrips, numTrisPerStrip);
+		
+		int numStrips = heightmapHeight - 1;
+		int numTrisPerStrip = (heightmapWidth - 1) * 2;
+
+		generateTerrainMesh(heightmap, heightmapWidth, heightmapHeight, heightmapHeight - 1, (heightmapWidth - 1) * 2);
 
 		// first, configure the cube's VAO (and terrainVBO + terrainIBO)
 		unsigned int terrainVAO, terrainVBO, terrainIBO;
@@ -187,17 +192,16 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Also clear the depth buffer now!
 
 		shaderProgram.Activate();
+
 		camera.Input(window);
 		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 		
 		glBindVertexArray(terrainVAO);
 		//        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		for (unsigned strip = 0; strip < numStrips; strip++)
+		for (int strip = 0; strip < heightmapHeight - 1; strip++)
 		{
-			glDrawElements(GL_TRIANGLE_STRIP,   // primitive type
-				numTrisPerStrip + 2,   // number of indices to render
-				GL_UNSIGNED_INT,     // index data type
-				(void*)(sizeof(unsigned) * (numTrisPerStrip + 2) * strip)); // offset to starting index
+			int startIndex = strip * (heightmapWidth - 1) * 2;
+			glDrawElements(GL_TRIANGLE_STRIP, ((heightmapWidth - 1) * 2) + 2, GL_UNSIGNED_INT, (void*)(startIndex * sizeof(unsigned)));
 		}
 
 		// Check events and swap the buffers
@@ -205,11 +209,11 @@ int main()
 		glfwPollEvents();
 	}
 	
-	
+	glDeleteVertexArrays(1, &terrainVAO);
+	glDeleteBuffers(1, &terrainVBO);
+	glDeleteBuffers(1, &terrainIBO);
 
 	glfwTerminate();
 	return 0;
-
-
 
 }
