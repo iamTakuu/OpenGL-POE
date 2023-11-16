6e35f57186fafaba0aef377edaa9f8a84b425a07
Taku <marowatk@gmail.com>
2023-11-16 21:01:29 +0200

    Knight || Queen || Rook

--- OpenGL-POE/src/Application.cpp
+++ OpenGL-POE/src/Application.cpp
@@ -29,56 +29,6 @@ bool camLocked = true;
 // 2 = Point
 int light_type = 2;
 
-// Callback function to resize the window
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-// Callback function to process input
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS) {
-		camLocked = !camLocked;
-	}	
-}
-
-bool InitWindow()
-{
-	glfwInit();
-
-	// Set the major and minor versions of OpenGL that we want to use
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-	// Set the profile of OpenGL that we want to use
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	window = glfwCreateWindow(width, height, "POE - Chess Demo", nullptr, nullptr);
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return true;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval( 0 );
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		std::cerr << "Failed to initialize GLEW." << std::endl;
-		return true;
-	}
-
-	glViewport(0, 0, width, height);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	return false;
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////		Test for Skybox		/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 float skybox_vertices[] =
 {
 	//Skybox coordinates
@@ -114,9 +64,54 @@ unsigned int skybox_indices[] =
 	6,2,3
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////		Test for Skybox		/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Callback function to resize the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+// Callback function to process input
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS) {
+		camLocked = !camLocked;
+	}	
+}
+
+// Function to initialize the GLFW window
+bool InitWindow()
+{
+	glfwInit();
+
+	// Set the major and minor versions of OpenGL that we want to use
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Set the profile of OpenGL that we want to use
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(width, height, "POE - Chess Demo", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return true;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval( 0 );
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW." << std::endl;
+		return true;
+	}
+
+	glViewport(0, 0, width, height);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	return false;
+}
 
 int main()
 {
@@ -124,26 +119,28 @@ int main()
 		return -1;
 
 	Shader default_shader = Shader("Shaders/default.vert", "Shaders/default.frag");
+	default_shader.Activate();
 	
 	// Take care of all the light related things.
 	// You can change the type of light in the shader
-	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-	glm::vec3 lightPos = glm::vec3(0.5f, 5.0f, -5.5f);
-	glm::mat4 lightModel = glm::mat4(1.0f);
-	lightModel = glm::translate(lightModel, lightPos);
+	glm::vec4 light_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 light_pos = glm::vec3(0.5f, 5.0f, -5.5f);
+	glm::mat4 light_model = glm::mat4(1.0f);
+	light_model = glm::translate(light_model, light_pos);
 
-	default_shader.Activate();
-	glUniform4f(glGetUniformLocation(default_shader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-	glUniform3f(glGetUniformLocation(default_shader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	// Set the light properties in the shader
+	glUniform4f(glGetUniformLocation(default_shader.ID, "lightColor"), light_color.x, light_color.y, light_color.z, light_color.w);
+	glUniform3f(glGetUniformLocation(default_shader.ID, "lightPos"), light_pos.x, light_pos.y, light_pos.z);
 	glUniform1i(glGetUniformLocation(default_shader.ID, "lightingType"), light_type);
 	
 
-	Shader skybox_Shader = Shader("Shaders/Skybox.vert", "Shaders/Skybox.frag");
-	Board board(64);
+	Shader skybox_shader = Shader("Shaders/Skybox.vert", "Shaders/Skybox.frag");
 	
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
 
+	// Create the board with 64 cubes
+	Board board(64);
 	// Camera
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 80.0f));
 
@@ -306,10 +303,21 @@ int main()
 	terrain.transform.setLocalScale(glm::vec3(0.5f, 0.5f, 0.5f));
 	terrain.transform.setLocalPosition(glm::vec3(0.0f, -4.5f, 0.0f));
 	
-	Model testModel ("Models/Rook.obj");
-	testModel.transform.setLocalPosition( glm::vec3(0.0f, 2.0f, 0.0f));
-	testModel.transform.setLocalRotation( glm::vec3(30.0f, 0.0f, -20.0f));
-	//testModel.transform.setLocalScale(glm::vec3( 2.0f, 2.0f, 2.0f));
+	Model rook_model ("Models/Rook.obj");
+	rook_model.transform.setLocalPosition( glm::vec3(0.0f, 4.0f, 0.0f));
+	rook_model.transform.setLocalRotation( glm::vec3(30.0f, 0.0f, -20.0f));
+
+	Model queen_model ("Models/Queen.obj");
+	queen_model.transform.setLocalPosition( glm::vec3(8.0f, 2.0f, -2.0f));
+	queen_model.transform.setLocalRotation( glm::vec3(-43.0f, 0.0f, 40.0f));
+
+	Model knight_model ("Models/Knight.obj");
+	knight_model.transform.setLocalPosition( glm::vec3(-5.0f, 3.8f, 4.0f));
+	knight_model.transform.setLocalRotation( glm::vec3(12.0f, 0.0f, 72.0f));
+
+
+	
+	
 	IMGUI_CHECKVERSION();
 	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
@@ -392,8 +400,8 @@ int main()
 		}
 	}
 
-	skybox_Shader.Activate();
-	glUniform1i(glGetUniformLocation(skybox_Shader.ID, "skybox"), 0);
+	skybox_shader.Activate();
+	glUniform1i(glGetUniformLocation(skybox_shader.ID, "skybox"), 0);
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////		Test for Skybox		/////////////////////////////////////////////////////////////////////////////////
@@ -419,12 +427,10 @@ int main()
 			
 			ImGui::End();
 		}
-		//ImGui::ShowDemoWindow(); // Show demo window! :)
 		// Input
 		processInput(window);
 		// ******************************* Rendering commands here ******************************************************
 		// Clear the screen to a specific color
-
 		//Clear color to black (not really necessary actually since we are drawing a background image)
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Also clear the depth buffer now!
@@ -433,7 +439,9 @@ int main()
 		camera.updateMatrix(20.0f, 0.1f, 200.0f);
 		camera.Inputs(window, camLocked);
 
-		testModel.Draw(default_shader, camera);
+		rook_model.Draw(default_shader, camera);
+		queen_model.Draw(default_shader, camera);
+		knight_model.Draw(default_shader, camera);
 
 // 		// For loop to render all the Pawn objects
 		 board.Draw(default_shader, camera);
@@ -488,14 +496,14 @@ int main()
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		glDepthFunc(GL_LEQUAL);
-		skybox_Shader.Activate();
+		skybox_shader.Activate();
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 		view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
 		projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
 
-		glUniformMatrix4fv(glGetUniformLocation(skybox_Shader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(glGetUniformLocation(skybox_Shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(skybox_shader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(skybox_shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 		// Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
 		// where an object is present (a depth of 1.0f will always fail against any object's depth value)
